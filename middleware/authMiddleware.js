8f4397636d939da8e8e345abd1f4fd5d3a411e8b
Sakshamebdev873 <vinodarya344@gmail.com>
2025-08-30 18:41:47 +0530

    Refactor authMiddleware to use Redis for token blacklisting and improve error handling

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,25 +1,23 @@
 import jwt from "jsonwebtoken";
 import { PrismaClient } from "../generated/prisma/client";
-
+import redisClient from '../libs/redisClient.js'
 const prisma = new PrismaClient();
 export async function authMiddleware(req, res, next) {
-  const token = req.headers.authorization?.spilt(" ")[1];
-  if (!token) {
-    return res.status(401).json({ error: "No token provided" });
-  }
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ error: "No token provided" });
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const blacklisted = await prisma.blacklistedToken.findUnique({
-      where: { jti: decoded.jti },
-    });
-    if (blacklisted) {
-      return res
-        .status(401)
-        .json({ error: "Token revoked, please login again..." });
+
+    // Check blacklist
+    const isBlacklisted = await redisClient.get(`blacklist:${decoded.jti}`);
+    if (isBlacklisted) {
+      return res.status(401).json({ error: "Token blacklisted. Please log in again." });
     }
-    req.user = decoded;
+
+    req.user = { id: decoded.userId };
     next();
-  } catch (error) {
-    return res.status(401).json({ error: "Invalid or expired token" });
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
   }
 }
